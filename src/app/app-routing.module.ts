2fbd3a2f4ccb0657bfb1422567132852671bf835
Susanta Kumar Sahoo <susanta@nvipani.com>
2018-10-22 19:01:32 +0530

    refactor app component and auth guard/service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
